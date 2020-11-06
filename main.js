5bfe06a461b49416f91ba14f7cf9783e297de701
Asyraaf Mac <ahmadasyraaf@gmail.com>
2020-11-06 11:32:33 +0800

    Database functions done!
    - Landing page now shows all buttons with all letters generated dynamically
    - Titles are now displayed with a limit of 10 per page, with functional next/previous page buttons that will only appear if there is a next/previous page
    - Some refactoring
    
    To Do:
    - Make titles clickable links to more information
    - API calls for the book reviews
    - Remove console logging

--- main.js
+++ main.js
@@ -39,7 +39,11 @@ const pool = mysql.createPool({
 })
 
 // SQL Queries
-const SQL_GET_TITLE = 'select title from goodreads.book2018 where title like ?'
+const SQL_GET_TITLE = 'select title from goodreads.book2018 where title like ? order by title asc limit 10 offset ?';
+const SQL_GET_TITLE_TOTAL = 'select count(*) as totalCount from goodreads.book2018 where title like ?'
+
+const letterArr = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const numArr = '0123456789'.split('');
 
 // SQL Query creator function
 const mkQuery = (sqlQuery, pool) => {
@@ -47,11 +51,9 @@ const mkQuery = (sqlQuery, pool) => {
         // Get connection from pool
         const conn = await pool.getConnection();
 
-        console.log('HELP ME ', await pool.query(sqlQuery, params));
-
         try {
             const results = await pool.query(sqlQuery, params)
-            console.log('THIS IS RESULTS: ', results);
+            console.log('THIS IS RESULTS: ', results[0]);
             return results[0];
         } catch (e) {
             return Promise.reject(e);
@@ -64,41 +66,75 @@ const mkQuery = (sqlQuery, pool) => {
 
 // SQL Query functions
 const getBookByTitle = mkQuery(SQL_GET_TITLE, pool);
+const getBookByTitleTotal = mkQuery(SQL_GET_TITLE_TOTAL, pool);
 
 // Request handlers
 // Homepage
 app.get('/', async (req, res) => {
     res.status(200);
     res.type('text/html');
-    res.render('index');
+    res.render('index', {
+        letterArr,
+        numArr
+    });
 });
 
 // Book Titles Page
 app.get('/titles/:titleStart', async (req, res) => {
 
-    const titleStart = req.params.titleStart;
+    const searchLetter = req.params.titleStart;
+    const titleStart = req.params.titleStart + '%';
+
+    // const conn = await pool.getConnection(); // TESTING
 
     console.log('this is from button: ', req.params.titleStart);
 
     try {
+        const setLimit = 10
+        const offsetBy = parseInt(req.query['offset']) || 0
+        let totalResults = await getBookByTitleTotal([titleStart])
+        totalResults = totalResults[0].totalCount;
 
-      const results = await getBookByTitle([titleStart]);
-      const recs = results[0];
+        const results = await getBookByTitle([titleStart, offsetBy]);
 
-      console.log('recs: ', recs);
+        // let results = await conn.query(SQL_GET_TITLE, 'a%'); //TESTING
+        // const recs = results[0];
 
-      if (recs.length <= 0) {
-          res.status(404);
-          res.type('text/html');
-          res.send(`${titleStart} not found`);
-          return
-      }
+        console.log('END RESULTS: ', results);
+        console.log('TOTAL RESULTS: ', totalResults);
+
+        // if (recs.length <= 0) {
+        //     res.status(404);
+        //     res.type('text/html');
+        //     res.send(`Books starting with ${searchLetter} not found`);
+        //     return
+        // }
+
+        const totalPages = Math.ceil(totalResults / setLimit)
+        console.info('number of pages: ', totalPages)
+
+        let page = offsetBy / setLimit + 1
+        console.info('offset', offsetBy)
+        console.info('limit: ', setLimit)
+        console.info('page: ', page)
+
+        const prevPage = (page > 1) ? 'Previous' : ''
+        const nextPage = (page < totalPages) ? 'Next' : ''
+
+        console.info('p:', prevPage)
+        console.info('n:', nextPage)
 
         res.status(200);
         res.type('text/html');
         res.render('titles', {
-          titleResults: recs
+            searchLetter,
+            titleResults: results,
+            prevPage: !!prevPage,
+            nextPage: !!nextPage,
+            prevOffset: offsetBy - setLimit,
+            nextOffset: offsetBy + setLimit
         });
+
     } catch (e) {
         console.info(e)
         res.status(500);
